12467dedfc7bbc138e3b0817aeec149dddf1bf6c
mattkrebs1974 <mattkrebsemail@gmail.com>
2021-09-11 08:52:58 -0400

    simple edit

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
+//Node needs to be installed.
+//Open "Terminal" window.
+//At the Terminal prompt type: node test
+//Press "enter". 
+//See result from the console logs.
+//Note: any javascript can be checked within the console.log
+
 console.log("1.",isNaN("Hopper"))
 console.log("2.",isNaN("123"))
 console.log("3.",isNaN("Hopper") && isNaN("r"))
