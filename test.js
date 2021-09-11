ab371abeae23cfbb3d09410516be7a84d0c0d01d
mattkrebs1974 <mattkrebsemail@gmail.com>
2021-09-11 09:03:54 -0400

    javascript with embedded console.logs

--- test.js
+++ test.js
@@ -5,8 +5,21 @@
 //See result from the console logs.
 //Note: any javascript can be checked within the console.log
 
-console.log("1.",isNaN("Hopper"))
+console.log("1.",!isNaN("age"))
 console.log("2.",isNaN("123"))
 console.log("3.",isNaN("Hopper") && isNaN("r"))
 console.log("4.",isNaN("Hopper") && isNaN("123"))
 console.log("5.",isNaN("Hopper") || isNaN("123"))
+
+
+const rate = 5;
+
+if ( isNaN(rate) ) {
+    console.log ("You did not provide a number for the rate.");
+} else if ( rate < 0 ) {
+    console.log ("The rate may not be less than zero.");
+} else if ( rate > 12 ) {
+    console.log ("The rate may not be greater than 12.");
+} else {
+    console.log ("The rate is: " + rate + ".");
+}
